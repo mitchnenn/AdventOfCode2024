e20e95a4516b03cfc232e1ab3991a61467d536e6
Mitch Nenn <mitch.nenn@earthlink.net>
2024-12-04 07:58:52 -0800

    Day 1 complete.

--- Day01/Program.fs
+++ Day01/Program.fs
@@ -2,18 +2,23 @@
 open SupportLib.ParseListPairs
 
 
-let exampleInput = readAllText "example_input.txt"
+//let input = readAllText "example_input.txt"
+let input = readAllText "problem_input.txt"
+
 let listPairs =
-    match parseListPairs exampleInput with
+    match parseListPairs input with
         | Result.Ok(records) -> records 
         | Result.Error(errorMsg) -> printfn $"List parse error: %s{errorMsg}"; []
         
 let left = getLeft listPairs |> List.sort 
 let right = getRight listPairs |> List.sort
-
-printfn $"%A{left}"
-printfn $"%A{right}"
-
 let calcDistance = List.map2 (-)
 let distances = calcDistance left right |> List.map(abs) |> List.sum 
 printfn $"%A{distances}"
+
+let HowManySatisfy pred = Seq.filter pred >> Seq.length 
+let similarityScore = List.map2(fun first _ ->
+    first, right |> HowManySatisfy(fun elem -> elem = first)) left right
+                    |> List.map (fun t -> fst(t) * snd(t))
+                    |> List.sum
+printfn $"%A{similarityScore}"
