b7368793f933659681aad973a2b67b2926f0db6f
Mitch Nenn <mitch.nenn@earthlink.net>
2024-12-10 22:36:02 -0800

    Day 3 part 2 complete.

---
+++ Day03/Program.fs
@@ -0,0 +1,31 @@
+﻿open FParsec
+open Microsoft.FSharp.Core
+open SupportLib.FileReading
+open SupportLib.FParsecSupport
+
+//let input = readAllText "example_input.txt"
+let input = readAllText "problem_input.txt"
+
+let firstParser: Parser<int, unit> =
+    str "mul(" >>. pint32
+    
+let secondParser: Parser<int, unit> =
+    str "," >>. pint32 .>> str ")"
+
+let mulOperandParser: Parser<int, unit> =
+    firstParser .>>. secondParser
+    |>> fun (first,second) -> first * second
+    
+let parseMultiplications (input: string) : int =
+    let rec loop (input:string) (enabled:bool) (acc:int) : int =
+        match input with
+        | _ when input.Length = 0 -> acc
+        | _ when input.StartsWith("don't()") -> loop (input.Substring(7)) false acc
+        | _ when input.StartsWith("do()") -> loop (input.Substring(4)) true acc
+        | _ -> let updatedAcc = match run mulOperandParser input with
+                                | Success (result,_,_) when enabled -> result + acc
+                                | _ -> acc
+               loop (input.Substring(1)) enabled updatedAcc
+    loop input true 0
+        
+printfn $"%d{parseMultiplications input}"
--- SupportLib/FParsecSupport.fs
+++ SupportLib/FParsecSupport.fs
@@ -1,9 +1,14 @@
 namespace SupportLib
 
-module FParsecTest =
+module FParsecSupport =
     open FParsec
-    
+
+    let str s = pstring s
+    let ws = spaces
+    let str_ws s = pstring s .>> ws
+
     let test p str =
         match run p str with
         | Success(result, _, _)   -> printfn $"Success: %A{result}"
         | Failure(errorMsg, _, _) -> printfn $"Failure: %s{errorMsg}"
+
