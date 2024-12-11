ba75294f0cedeb4a4f9020d731c54b69f854928b
Mitch Nenn <mitch.nenn@earthlink.net>
2024-12-11 07:33:55 -0800

    Some Day 3 cleanup.

--- Day03/Program.fs
+++ Day03/Program.fs
@@ -6,8 +6,12 @@ open SupportLib.FParsecSupport
 //let input = readAllText "example_input.txt"
 let input = readAllText "problem_input.txt"
 
+let mulInst = "mul("
+let dontInst = "don't()"
+let doInst = "do()"
+
 let firstParser: Parser<int, unit> =
-    str "mul(" >>. pint32
+    str mulInst >>. pint32
     
 let secondParser: Parser<int, unit> =
     str "," >>. pint32 .>> str ")"
@@ -20,8 +24,8 @@ let parseMultiplications (input: string) : int =
     let rec loop (input:string) (enabled:bool) (acc:int) : int =
         match input with
         | _ when input.Length = 0 -> acc
-        | _ when input.StartsWith("don't()") -> loop (input.Substring(7)) false acc
-        | _ when input.StartsWith("do()") -> loop (input.Substring(4)) true acc
+        | _ when input.StartsWith(dontInst) -> loop (input.Substring(dontInst.Length)) false acc
+        | _ when input.StartsWith(doInst) -> loop (input.Substring(doInst.Length)) true acc
         | _ -> let updatedAcc = match run mulOperandParser input with
                                 | Success (result,_,_) when enabled -> result + acc
                                 | _ -> acc
